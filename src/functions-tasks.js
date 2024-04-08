7be420c33fbe56775ea0b5313202139b42b5eefe
github-mariyaweb <96mariyask@gmail.com>
2024-04-08 17:41:04 +0300

    feat: add 6 task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -123,8 +123,18 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+  function memo(...args) {
+    const obj = JSON.stringify(args);
+    if (cache[obj] !== undefined) {
+      return cache[obj];
+    }
+    const res = func(...args);
+    cache[obj] = res;
+    return res;
+  }
+  return memo;
 }
 
 /**
